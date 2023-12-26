1c5079d41a23b3c925658efb1dbfb81e6f54da3b
pietrobrighella <info@pietrobrighella.com>
2023-12-26 14:51:01 +0000

    auth fix

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <Authenticator>
+      <template v-slot="{ user, signOut }">
+        <h1>Hello {{ user.username }}!</h1>
+        <div>
+          <NuxtWelcome />
+        </div>
+        <button @click="signOut">Sign Out</button>
+      </template>
+    </Authenticator>
   </div>
 </template>
